a238b06c171d654ea1b745b7537b986f8c0169a9
Harun Taha Kepenek <hakukpnk@gmail.com>
2024-02-23 13:15:39 +0300

    swagger

--- E-Commerce.API/Program.cs
+++ E-Commerce.API/Program.cs
@@ -110,11 +110,9 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
